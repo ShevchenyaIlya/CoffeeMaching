0f6b2ebc979435ebfeed0a1515d692a048be3a8c
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-01-26 22:12:24 +0300

    Add feach to catkh some exceptions.

--- MainFile.py
+++ MainFile.py
@@ -11,31 +11,37 @@ def current_supply():
 
 def buy():
     global coffee, water, money, milk, cups
-    case = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappucino: "))
-    if case == 1:
-        coffee -= 16
-        water -= 250
-        money += 4
-    elif case == 2:
-        coffee -= 20
-        water -= 350
-        milk -= 75
-        money += 7
-    elif case == 3:
-        coffee -= 12
-        water -= 200
-        milk -= 100
-        money += 6
-    cups -= 1
+    case = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: "))
+    if coffee >= 0 and water >= 0 and water >= 0 and milk >= 0:
+        if case == 1:
+            coffee -= 16
+            water -= 250
+            money += 4
+        elif case == 2:
+            coffee -= 20
+            water -= 350
+            milk -= 75
+            money += 7
+        elif case == 3:
+            coffee -= 12
+            water -= 200
+            milk -= 100
+            money += 6
+        cups -= 1
+    else:
+        print("There is no need supply")
     current_supply()
 
 
 def fill():
     global coffee, water, money, milk, cups
-    water += int(input("Write how many ml of water do you want to add: "))
-    milk += int(input("Write how many ml of milk do you want to add: "))
-    coffee += int(input("Write how many grams of coffee beans do you want to add: "))
-    cups += int(input("Write how many disposable cups of coffee do you want to add: "))
+    try:
+        water += int(input("Write how many ml of water do you want to add: "))
+        milk += int(input("Write how many ml of milk do you want to add: "))
+        coffee += int(input("Write how many grams of coffee beans do you want to add: "))
+        cups += int(input("Write how many disposable cups of coffee do you want to add: "))
+    except ValueError:
+        print("Sorry, there is exception. Incorrect input.")
     current_supply()
 
 
